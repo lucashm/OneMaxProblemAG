7830e9c22f3a626731830d6ecfb780b40ef383e2
Lucas <lucas50xd@hotmail.com>
2017-06-28 15:11:06 -0300

    adding onClick call function

--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      test: 2
+    };
+  }
+
+  testing = (a) => {
+    this.setState({
+      test: a
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +25,14 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <button onClick={() => this.testing(5)}>
+          Click me
+        </button>
+
+        <p>{this.state.test}</p>
+
+
       </div>
     );
   }
